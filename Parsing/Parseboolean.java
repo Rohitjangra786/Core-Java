e2130d8fbc56acfa5fa15dd75854e3e7c17b234b
Rohit Kumar <127589000+Rohitjangra786@users.noreply.github.com>
2024-08-27 13:50:30 +0530

    commit

---
+++ Parsing/Parseboolean.java
@@ -0,0 +1,11 @@
+package Parsing;
+
+public class parseboolean {
+    public static void main(String[] args) {
+        String str = "true";
+        boolean b = Boolean.parseBoolean(str);
+
+        System.out.println(b);
+        
+    }
+}
